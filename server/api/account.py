aca5cb8f26ffb1fc440f3c783eb4234868255086
i-am-unknown-81514525 <74453352+i-am-unknown-81514525@users.noreply.github.com>
2025-10-04 18:20:52 +0100

    Add account list API

--- server/api/account.py
+++ server/api/account.py
@@ -1,7 +1,13 @@
 from typing import Annotated
 
+import asqlite
 from fastapi import FastAPI, APIRouter, Depends
+from fastapi.responses import JSONResponse
+
+from schema.db import Account
 from helper.jwt_helper import get_user
+from helper.db_helper import get_tx_conn
+from database.account import get_raw_user_account
 
 acc_app = FastAPI()
 
@@ -9,9 +15,16 @@ public_router = APIRouter()
 protected_router = APIRouter(dependencies=[Depends(get_user)])
 
 
-@public_router.get("/list/@me")
-async def list_auth_self_accounts(user_id: Annotated[int, Depends(get_user)]): ...
+@protected_router.get("/list/@me")
+async def list_auth_self_accounts(
+    conn: Annotated[asqlite.ProxiedConnection, Depends(get_tx_conn)],
+    user_id: Annotated[int, Depends(get_user)],
+) -> list[Account]:
+    return await get_raw_user_account(conn, user_id)
 
 
-@protected_router.get("/list/{user_id:int}")
-async def list_user_accounts(user_id: int): ...
+@public_router.get("/list/{user_id:int}")
+async def list_user_accounts(
+    conn: Annotated[asqlite.ProxiedConnection, Depends(get_tx_conn)], user_id: int
+) -> list[Account]:
+    return await get_raw_user_account(conn, user_id)
